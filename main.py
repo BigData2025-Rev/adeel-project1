2a81f4e3b55505a28e82e0291f760f2e03930c7c
hussain-adeel <adeel@adeelh.me>
2025-01-07 12:51:12 -0800

    Testing, gitignore changes for pyc files

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from models import Account
+
 def main():
-    pass
+    #(self, first_name, last_name, email, password, address, city, state, zip, points = 100, admin = False, _id = None)
+    newAcc = Account("Test", "Account", "test@test.com", "password", "123 Test St", "Test City", "TX", "12345")
+    print(newAcc)
+    dictAcc = newAcc.to_dict()
+    print(dictAcc)
+    print(Account.from_dict(Account, dictAcc))
 
 if __name__ == "__main__":
     main()
